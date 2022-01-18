a3d97fb5c530cd004a9ed7846fc9e7c1233a0982
toeyeen <oluwatoyinjolaoso@gmail.com>
2022-01-18 14:50:11 +0100

    Project setup

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="toyin">
+    <h1>Navbar</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.toyin {
+  @apply bg-green-500 text-white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+export default {};
+</script>
